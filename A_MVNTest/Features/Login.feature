ac005666566701b25c4e97d3b3f99b5af0524fb7
veera <prathap.ufttest@gmail.com>
2017-05-26 11:23:48 +0530

    intial coomit

---
+++ A_MVNTest/Features/Login.feature
@@ -0,0 +1,4 @@
+Feature: Testing of login feature of the application
+
+Scenario: scenario description
+    Given Launch the ie browser
---
+++ A_MVNTest/src/test/java/com/Login/Login.java
@@ -0,0 +1,17 @@
+package com.Login;
+
+import cucumber.api.java.en.Given;
+
+public class Login {
+	
+	
+	@Given("Launch the ie browser")
+	
+	public static void launchtest()
+	{
+		
+		
+		System.out.println(" Launching ie browser");
+	}
+
+}
